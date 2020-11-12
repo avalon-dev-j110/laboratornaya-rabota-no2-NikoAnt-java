8188ca58f63905e5d3a2b7c5db90f9dcb8e46094
NikoAnt-java <73140639+NikoAnt-java@users.noreply.github.com>
2020-11-12 14:31:18 +0300

    new commit На проверку

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -3,16 +3,49 @@ package ru.avalon.java.dev.j10.labs.shapes;
 /**
  * Представление об окружности.
  * <p>
- * Окру́жность — замкнутая плоская кривая, которая состоит из
- * всех точек на плоскости, равноудалённых от заданной точки.
+ * Окру́жность — замкнутая плоская кривая, которая состоит из всех точек на
+ * плоскости, равноудалённых от заданной точки.
  *
- * @see <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
+ * @see
+ * <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
  */
-public class Circle {
+public class Circle implements Ellipse {
 
     /*
      * TODO: Реализовать класс 'Circle'
      * 1. Используйте наследование.
      * 2. Реализуйте все абстрактные методы.
      */
+    private float R;// радиус окружности
+    private float SCircle;// площадь окружности
+    private float LCircle;// длина окружности
+
+    public Circle(float R) {
+        this.R = R;
+    }
+
+    public float getR() {
+        return R;
+    }
+
+    public void setR(float R) {
+        this.R = R;
+    }
+
+    @Override
+    public float getLength() {
+        return LCircle = (float) (2 * Math.PI * R);
+    }
+
+    @Override
+    public float getArea() {
+        return SCircle = (float) (Math.PI * Math.pow(R, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Окружность ";
+//        final String Circle = "Окружность: площадь = ";
+//        return Circle;
+    }
 }
--- src/ru/avalon/java/dev/j10/labs/shapes/Triangle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Triangle.java
@@ -3,19 +3,70 @@ package ru.avalon.java.dev.j10.labs.shapes;
 /**
  * Представление о треугольнике.
  * <p>
- * Треуго́льник (в евклидовом пространстве) — геометрическая
- * фигура, образованная тремя отрезками, которые соединяют
- * три точки, не лежащие на одной прямой. Указанные три
- * точки называются вершинами треугольника, а отрезки —
- * сторонами треугольника. Часть плоскости, ограниченная
- * сторонами, называется внутренностью треугольника: нередко
- * треугольник рассматривается вместе со своей внутренностью
- * (например, для определения понятия площади).
+ * Треуго́льник (в евклидовом пространстве) — геометрическая фигура,
+ * образованная тремя отрезками, которые соединяют три точки, не лежащие на
+ * одной прямой. Указанные три точки называются вершинами треугольника, а
+ * отрезки — сторонами треугольника. Часть плоскости, ограниченная сторонами,
+ * называется внутренностью треугольника: нередко треугольник рассматривается
+ * вместе со своей внутренностью (например, для определения понятия площади).
  *
- * @see <a href="https://ru.wikipedia.org/wiki/%D0%A2%D1%80%D0%B5%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Треугольник</a>
+ * @see
+ * <a href="https://ru.wikipedia.org/wiki/%D0%A2%D1%80%D0%B5%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Треугольник</a>
  */
-public class Triangle {
+public class Triangle implements Polygon {
 
+    private String Triangle;// название фигуры
+    private float x;//основание
+    private float h;//высота в треугольнике, длина отрезка перпендикулярного основанию
+    private float g;//гипотенуза
+    private float STriangle;//площадь треугольника
+    private float PTriangle;//периметр треугольника
+
+    public Triangle(float x, float h, float g) {
+        this.x = x;
+        this.h = h;
+        this.g = g;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getH() {
+        return h;
+    }
+
+    public void setH(float h) {
+        this.h = h;
+    }
+
+    public float getG() {
+        return g;
+    }
+
+    public void setG(float g) {
+        this.g = g;
+    }
+
+    
+
+    @Override
+    public float getPerimeter() {
+        return PTriangle = (float) (x + h + g);
+    }
+
+    @Override
+    public float getArea() {
+        return STriangle = (float) ((x * h) / 2);
+    }
+    @Override
+    public String toString() {
+        return "Треугольник ";
+    }
     /*
      * TODO: Реализовать класс 'Triangle'
      * 1. Используйте наследование.
